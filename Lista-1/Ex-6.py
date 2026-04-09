037547af1dc703124e7b123033301c434cb8124b
NicolasRibeiroInacio <nicolas.inacio100410@gmail.com>
2026-04-09 20:36:34 -0300

    adicionando novos exercicios lista 1

---
+++ Lista-1/Ex-6.py
@@ -0,0 +1,5 @@
+dis = float(input('digite a distancia percorrida em km: '))
+v = float(input('digite a velocidade: '))
+
+vm = (dis/v)
+print(f'sua velocidade média é de {vm:.2f}')
---
+++ Lista-1/Ex-7.py
@@ -0,0 +1,3 @@
+C = int(input('digite a temperatura: '))
+F = 9*(C)/5 + 32
+print(f' a temperatura em farenheit é {F}')
